cce743d55344d26dc5e8e8290c779a4d594e6017
TahmidShawn7 <md.tahmid022@gmail.com>
2024-10-20 15:07:42 +0600

    feat: add comment model

--- server/src/models/comment.model.js
+++ server/src/models/comment.model.js
@@ -1,5 +1,23 @@
 import mongoose from "mongoose";
 
-const commentSchema = new mongoose.Schema();
+const commentSchema = new mongoose.Schema(
+    {
+        userId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true,
+        },
+        comment: {
+            type: String,
+            required: true,
+            maxLength: [200, "Comment cannot exceed 200 characters"],
+        },
+        postedAt: {
+            type: Date,
+            default: Date.now,
+        },
+    },
+    { _id: false }
+);
 
 export default commentSchema;
