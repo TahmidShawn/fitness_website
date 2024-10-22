e0b81068d852dad813e4e4a6335c2d9ebff6fd67
TahmidShawn7 <md.tahmid022@gmail.com>
2024-10-22 21:53:17 +0600

    feat: add category section

---
+++ client/src/pages/home/category/Category.jsx
@@ -0,0 +1,166 @@
+import { CornerDownRight } from "lucide-react";
+
+const Category = () => {
+    return (
+        <div className="max-w-6xl mx-auto px-2 py-12">
+            <h2 className="text-4xl text-center font-semibold tracking-wide mb-2">
+                Lorem ipsum dolor sit.
+            </h2>
+            <p className="text-center tracking-widest">
+                Join now and start your journey to a stronger, fitter you!
+            </p>
+            {/* category card */}
+            <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3 mt-10">
+                {/* card 1  */}
+                <div className="bg-white pb-5">
+                    <img
+                        src="https://images.unsplash.com/photo-1607962837359-5e7e89f86776?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8Z3JvdXAlMjBmaXRuZXNzfGVufDB8fDB8fHwy"
+                        alt=""
+                    />
+                    <div className="px-4 ml-1">
+                        <h2 className="text-base md:text-xl font-medium mt-4 mb-1">
+                            Purposeful Training
+                        </h2>
+                        <p className="text-sm tracking-wide truncate">
+                            Focus on the right exercises for maximum impact.
+                        </p>
+                        <div className="flex justify-end mt-7">
+                            <button className="flex justify-center items-center gap-2 font-semibold">
+                                <CornerDownRight className="bg-primaryColor w-8 h-8 p-1 rounded-md" />
+                                Start Now
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                {/* card 1  */}
+                <div className="bg-white pb-5">
+                    <img
+                        src="https://images.unsplash.com/photo-1607962837359-5e7e89f86776?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8Z3JvdXAlMjBmaXRuZXNzfGVufDB8fDB8fHwy"
+                        alt=""
+                    />
+                    <div className="px-4 ml-1">
+                        <h2 className="text-xl font-medium mt-4 mb-1">
+                            Purposeful Training
+                        </h2>
+                        <p className="text-sm tracking-wide truncate">
+                            Focus on the right exercises for maximum impact.
+                        </p>
+                        <div className="flex justify-end mt-7">
+                            <button className="flex justify-center items-center gap-2 font-semibold">
+                                <CornerDownRight className="bg-primaryColor w-8 h-8 p-1 rounded-md" />
+                                Start Now
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                {/* card 1  */}
+                <div className="bg-white pb-5">
+                    <img
+                        src="https://images.unsplash.com/photo-1607962837359-5e7e89f86776?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8Z3JvdXAlMjBmaXRuZXNzfGVufDB8fDB8fHwy"
+                        alt=""
+                    />
+                    <div className="px-4 ml-1">
+                        <h2 className="text-xl font-medium mt-4 mb-1">
+                            Purposeful Training
+                        </h2>
+                        <p className="text-sm tracking-wide truncate">
+                            Focus on the right exercises for maximum impact.
+                        </p>
+                        <div className="flex justify-end mt-7">
+                            <button className="flex justify-center items-center gap-2 font-semibold">
+                                <CornerDownRight className="bg-primaryColor w-8 h-8 p-1 rounded-md" />
+                                Start Now
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                {/* card 1  */}
+                <div className="bg-white pb-5">
+                    <img
+                        src="https://images.unsplash.com/photo-1607962837359-5e7e89f86776?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8Z3JvdXAlMjBmaXRuZXNzfGVufDB8fDB8fHwy"
+                        alt=""
+                    />
+                    <div className="px-4 ml-1">
+                        <h2 className="text-xl font-medium mt-4 mb-1">
+                            Purposeful Training
+                        </h2>
+                        <p className="text-sm tracking-wide truncate">
+                            Focus on the right exercises for maximum impact.
+                        </p>
+                        <div className="flex justify-end mt-7">
+                            <button className="flex justify-center items-center gap-2 font-semibold">
+                                <CornerDownRight className="bg-primaryColor w-8 h-8 p-1 rounded-md" />
+                                Start Now
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                {/* card 1  */}
+                <div className="bg-white pb-5">
+                    <img
+                        src="https://images.unsplash.com/photo-1607962837359-5e7e89f86776?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8Z3JvdXAlMjBmaXRuZXNzfGVufDB8fDB8fHwy"
+                        alt=""
+                    />
+                    <div className="px-4 ml-1">
+                        <h2 className="text-xl font-medium mt-4 mb-1">
+                            Purposeful Training
+                        </h2>
+                        <p className="text-sm tracking-wide truncate">
+                            Focus on the right exercises for maximum impact.
+                        </p>
+                        <div className="flex justify-end mt-7">
+                            <button className="flex justify-center items-center gap-2 font-semibold">
+                                <CornerDownRight className="bg-primaryColor w-8 h-8 p-1 rounded-md" />
+                                Start Now
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                {/* card 1  */}
+                <div className="bg-white pb-5">
+                    <img
+                        src="https://images.unsplash.com/photo-1607962837359-5e7e89f86776?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8Z3JvdXAlMjBmaXRuZXNzfGVufDB8fDB8fHwy"
+                        alt=""
+                    />
+                    <div className="px-4 ml-1">
+                        <h2 className="text-xl font-medium mt-4 mb-1">
+                            Purposeful Training
+                        </h2>
+                        <p className="text-sm tracking-wide truncate">
+                            Focus on the right exercises for maximum impact.
+                        </p>
+                        <div className="flex justify-end mt-7">
+                            <button className="flex justify-center items-center gap-2 font-semibold">
+                                <CornerDownRight className="bg-primaryColor w-8 h-8 p-1 rounded-md" />
+                                Start Now
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                {/* card 1  */}
+                <div className="bg-white pb-5">
+                    <img
+                        src="https://images.unsplash.com/photo-1607962837359-5e7e89f86776?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8Z3JvdXAlMjBmaXRuZXNzfGVufDB8fDB8fHwy"
+                        alt=""
+                    />
+                    <div className="px-4 ml-1">
+                        <h2 className="text-xl font-medium mt-4 mb-1">
+                            Purposeful Training
+                        </h2>
+                        <p className="text-sm tracking-wide truncate">
+                            Focus on the right exercises for maximum impact.
+                        </p>
+                        <div className="flex justify-end mt-7">
+                            <button className="flex justify-center items-center gap-2 font-semibold">
+                                <CornerDownRight className="bg-primaryColor w-8 h-8 p-1 rounded-md" />
+                                Start Now
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Category;
