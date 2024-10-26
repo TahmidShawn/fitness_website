1131d709aaa20a4ab42ab458dc1ae3ce48004794
TahmidShawn7 <md.tahmid022@gmail.com>
2024-10-26 12:09:14 +0600

    feat: design class card and filter section

--- client/src/pages/programs/Programs.jsx
+++ client/src/pages/programs/Programs.jsx
@@ -5,20 +5,30 @@ import { useState } from "react";
 
 const Programs = () => {
     const [showFilter, setShowFilter] = useState(false);
+    const [category, setCategory] = useState([]);
 
     const endpoint = "/api/v1/class";
     const { data, isLoading } = useDataFetch(endpoint);
-    console.log(data);
+
+    const toggleCategory = (e) => {
+        if (category.includes(e.target.value)) {
+            setCategory((prev) =>
+                prev.filter((item) => item !== e.target.value)
+            );
+        } else {
+            setCategory((prev) => [...prev, e.target.value]);
+        }
+    };
 
     if (isLoading) {
         return <p>Loading...</p>;
     }
     return (
         <div>
-            <div className="flex flex-col sm:flex-row gap-1 sm:gap-10  pt-10 border-t">
+            <div className="flex flex-col lg:flex-row gap-1 lg:gap-10 pt-10 border-t max-w-6xl mx-auto px-2">
                 {/* Filter Option */}
-                <div className="min-w-60 ">
-                    <div className="flex items-center gap-1">
+                <div className="min-w-60 lg:block">
+                    <div className="flex items-center gap-1 lg:hidden">
                         <p
                             onClick={() => {
                                 setShowFilter(!showFilter);
@@ -28,42 +38,21 @@ const Programs = () => {
                             FILTERS
                         </p>
                         <ChevronRight
-                            className={`h-4 sm:hidden ${
-                                showFilter ? "rotate-90" : ""
-                            }`}
+                            className={`h-4 ${showFilter ? "rotate-90" : ""}`}
                         />
                     </div>
+
                     {/* Category Filter */}
                     <div
-                        className={`border border-gray-300 pl-5 py-3 mt-6 ${
-                            showFilter ? "" : "hidden "
-                        }  sm:block`}
+                        className={`border border-gray-300 pl-5 py-3 ${
+                            showFilter ? "" : "hidden"
+                        } lg:block`}
                     >
                         <p className="mb-3 font-medium text-sm">Categories</p>
-                        <div className="flex flex-col gap-2 text-sm font-light text-gray-700">
-                            {/* first filter here  */}
-                        </div>
-                    </div>
-
-                    {/* Sub Category Filter */}
-                    <div
-                        className={`border border-gray-300 pl-5 py-3 my-5 ${
-                            showFilter ? "" : "hidden "
-                        }  sm:block`}
-                    >
-                        <p className="mb-3 font-medium text-sm">Type</p>
-                        <div className="flex flex-col gap-2 text-sm font-light text-gray-700">
-                            {/* second filter here  */}
-                        </div>
-                    </div>
-                </div>
-
-                <div className="flex-1">
-                    <div className="flex justify-between text-base sm:text-2xl mb-4">
                         <select
                             name=""
                             id=""
-                            className="border rounded-lg border-gray-600 text-sm px-2"
+                            className="border rounded-lg border-gray-600 text-sm py-2 px-2"
                         >
                             <option value="relevant">Sort by: Relevant</option>
                             <option value="low-high">
@@ -74,18 +63,60 @@ const Programs = () => {
                             </option>
                         </select>
                     </div>
+
+                    {/* Sub Category Filter */}
+                    <div
+                        className={`border border-gray-300 pl-5 py-3 my-5 ${
+                            showFilter ? "" : "hidden"
+                        } lg:block`}
+                    >
+                        <p className="mb-3 font-medium text-sm">Type</p>
+                        <div className="flex flex-col gap-2 text-sm font-light text-gray-700">
+                            <p className="flex gap-2">
+                                <input
+                                    onChange={toggleCategory}
+                                    type="checkbox"
+                                    name=""
+                                    id=""
+                                    value={"Cardio"}
+                                    className="w-3"
+                                />{" "}
+                                Cardio
+                            </p>
+                            <p className="flex gap-2">
+                                <input
+                                    onChange={toggleCategory}
+                                    type="checkbox"
+                                    name=""
+                                    id=""
+                                    value={"Yoga"}
+                                    className="w-3"
+                                />{" "}
+                                Yoga
+                            </p>
+                            <p className="flex gap-2">
+                                <input
+                                    onChange={toggleCategory}
+                                    type="checkbox"
+                                    name=""
+                                    id=""
+                                    value={"Gym"}
+                                    className="w-3"
+                                />{" "}
+                                Gym
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="flex-1">
                     {/* All Products */}
-                    <div className="mt-8">
-                        {
-                            <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 gap-y-6">
-                                {data.map((program, index) => (
-                                    <ProgramClass
-                                        key={index}
-                                        program={program}
-                                    />
-                                ))}
-                            </div>
-                        }
+                    <div className="mt-0">
+                        <div className="grid grid-cols-2 md:grid-cols-3 gap-4 gap-y-6">
+                            {data?.map((program, index) => (
+                                <ProgramClass key={index} program={program} />
+                            ))}
+                        </div>
                     </div>
                 </div>
             </div>
--- client/src/pages/programs/programClass/ProgramClass.jsx
+++ client/src/pages/programs/programClass/ProgramClass.jsx
@@ -18,7 +18,26 @@ const ProgramClass = ({ program }) => {
 
     return (
         <div className="">
-            <p>Hello</p>
+            <div className="bg-white shadow-[0_4px_12px_-5px_rgba(0,0,0,0.4)] w-full max-w-sm rounded-lg overflow-hidden mx-auto">
+                <div className="">
+                    <img src={images[0]} className="w-full h-48 object-cover" />
+                </div>
+
+                <div className="p-6">
+                    <h3 className="text-gray-800 text-xl font-bold truncate">
+                        {className}
+                    </h3>
+                    <p className="mt-4 text-sm text-gray-500 leading-relaxed line-clamp-2">
+                        {description}
+                    </p>
+                    <button
+                        type="button"
+                        className="mt-6 px-5 py-2.5 rounded-lg text-white text-sm tracking-wider border-none outline-none bg-blue-600 hover:bg-blue-700 active:bg-blue-600"
+                    >
+                        View
+                    </button>
+                </div>
+            </div>
         </div>
     );
 };
